d326f21bd18ddc12f5eabdc89201a8e82787ed65
Loki Jiang <summer10920@gmail.com>
2025-05-30 17:59:26 +0800

    clear project without tailwind

--- apps/loki_angular/src/app/app.component.ts
+++ apps/loki_angular/src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ CommonModule, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'loki_angular';
+
+  constructor(private renderer: Renderer2) {
+    const isDark = localStorage.getItem('darkMode') === 'true';
+    this.setDarkMode(isDark);
+  }
+
+
+  toggleDarkMode(): void {
+    const html = document.documentElement;
+    const isDark = html.classList.contains('dark');
+    this.setDarkMode(!isDark);
+  }
+
+  private setDarkMode(enable: boolean): void {
+    const html = document.documentElement;
+    if (enable) {
+      this.renderer.addClass(html, 'dark');
+    } else {
+      this.renderer.removeClass(html, 'dark');
+    }
+    localStorage.setItem('darkMode', enable.toString());
+  }
 }
