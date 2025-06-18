4f57adfca3d3e1dd7ae1fd4ab766125aca89b4a5
Loki Jiang <summer10920@gmail.com>
2025-06-18 23:11:35 +0800

    move angular to cli

--- apps/loki-angular-cli/src/app/app.component.ts
+++ apps/loki-angular-cli/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutsComponent } from './layouts/layouts.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [LayoutsComponent, RouterModule],
   selector: 'loki-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/loki-angular-cli/src/app/app.routes.ts
+++ apps/loki-angular-cli/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'learn',
+    loadChildren: () =>
+      import('./pages/learn/learn.routes').then((m) => m.LearnRoutes),
+  },
+];
