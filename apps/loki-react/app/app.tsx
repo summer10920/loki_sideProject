303054ac2a07107765c7a2822eec00e6222d7e5a
Loki Jiang <summer10920@gmail.com>
2025-06-19 19:19:52 +0800

    react done

---
+++ apps/loki-react/app/app.tsx
@@ -0,0 +1,15 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome loki-react 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
--- apps/loki-react/vite.config.ts
+++ apps/loki-react/vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../node_modules/.vite/loki-react',
+  cacheDir: '../../node_modules/.vite/apps/loki-react',
   server: {
     port: 4200,
     host: 'localhost',
@@ -15,28 +15,24 @@ export default defineConfig(() => ({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [
+    !process.env.VITEST && reactRouter(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+  ],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: '../dist/loki-react',
+    outDir: '../../dist/apps/loki-react',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
   },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../coverage/loki-react',
-      provider: 'v8' as const,
-    },
+  define: {
+    'import.meta.vitest': undefined,
   },
 }));
